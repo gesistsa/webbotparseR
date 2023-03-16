2cc02f27601e1d32a3a051d2f785d2212f529f24
schochastics <david@schochastics.net>
2023-03-16 16:40:08 +0100

    added rudimentary parsing

--- R/parser.R
+++ R/parser.R
@@ -1,4 +1,35 @@
-.parse_search_results <- function(doc,selector){
+#' Parse search engine results
+#'
+#' @param x, either a path to a file that contains search results, a path to a directory containing search engine result files, or a xml_document containing search engine results
+#' @param engine character.
+#' @param ..., not used
+#' @return a tibble of parsed search engine results
+#' @export
+parse_search_results <- function(x,engine, ...) {
+    UseMethod("parse_search_results", object = x,engine)
+}
+
+#' @rdname parse_search_results
+#' @export
+parse_search_results.character <- function(x,engine){
+    if(file.exists(x)){
+        doc <- rvest::read_html(x)
+        .parse_search_results(doc,engine)
+    } else if(dir.exists(x)){
+
+    }
+
+}
+
+#' @rdname parse_search_results
+#' @export
+parse_search_results.xml_document <- function(doc,engine){
+    .parse_search_page(doc,engine)
+}
+
+.parse_search_page <- function(doc,engine){
+    engine <- match.arg(engine,names(selectors))
+    selector <- selectors[["engine"]]
     results_selector <- selector[["results"]][1]
     if(is.null(results_selector)){
         stop("no search result selector found.")
